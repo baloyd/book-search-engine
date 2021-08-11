ea7e4203452e67661bbc4683840a6a91efc88fb7
baloyd <danielbaloy@gmail.com>
2021-08-11 13:31:58 -0400

    revert server

--- server/server.js
+++ server/server.js
@@ -7,25 +7,14 @@ const { authMiddleware } = require('./utils/auth');
 //const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
-
-async function startApolloServer(typeDefs, resolvers) {
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-await server.start();
-server.applyMiddleware({ app, path:'/' });
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
-};
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -39,3 +28,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
