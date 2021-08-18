ecb1c2338b6522b12c3a896d9ece1c237a9bb298
baloyd <danielbaloy@gmail.com>
2021-08-18 01:09:37 -0400

    login works;books saved;property length undefined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,7 +40,7 @@ type Book {
 
   type Mutation {
     login(email: String!, password: String!): Auth
-    createUser(username: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     saveBook(bookData: BookInput!):User
     deleteBook(bookId: ID!):User
   }
