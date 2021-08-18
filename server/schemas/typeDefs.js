86bbc22effbf133aedb84353ab222eab7dfe834d
baloyd <danielbaloy@gmail.com>
2021-08-18 00:19:22 -0400

    cant sign in

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,7 +41,7 @@ type Book {
   type Mutation {
     login(email: String!, password: String!): Auth
     createUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookData: BookInput!):User
+    saveBook(bookData: BookInput):User
     deleteBook(bookId: ID!):User
   }
 
